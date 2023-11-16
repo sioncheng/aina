7353e644a512dcc997d993c3dcd10525e6fe08d4
cyq <sion@cyqm2.local>
2023-11-16 10:26:25 +0800

    simplequestion

--- chapter03/src/main/scala/simplequestion/Worker.scala
+++ chapter03/src/main/scala/simplequestion/Worker.scala
@@ -8,9 +8,29 @@ import scala.util.Random
 
 object Worker {
 
-    sealed trait Command
-    final case class Parse(replyTo: ActorRef[Worker.Response]) extends Command
+  sealed trait Command
+  final case class Parse(replyTo: ActorRef[Worker.Response])
+      extends Command
 
-    sealed trait Response
-    final case object Done extends Response
+  sealed trait Response
+  final case object Done extends Response
+
+  def apply(text: String): Behavior[Command] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case Parse(replyTo) =>
+          fakeLengthyParsing(text)
+          context.log.info(s"${context.self.path.name}: done")
+          replyTo ! Worker.Done
+          Behaviors.same
+      }
+    }
+
+  private def fakeLengthyParsing(text: String): Unit = {
+    val endTime =
+      System.currentTimeMillis + Random.between(2000, 4000)
+    while (endTime > System.currentTimeMillis) {
+      Thread.sleep(1000)
+    }
+  }
 }
